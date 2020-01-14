585ac8960788cf68bca14ebcd888b54ce116da7b
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-01-14 21:21:57 +0100

    Test results

--- src/utils/primeMultiples.test.js
+++ src/utils/primeMultiples.test.js
@@ -1,6 +1,31 @@
 import { generatePrimeMultiples } from './primeMultiples';
 
 test('Return the multiples of primes', () => {
-  let multiples = generatePrimeMultiples(5);
-  console.log(multiples);
+  let multiples = generatePrimeMultiples(2);
+  let result = [
+    [1, 2, 3],
+    [2, 4, 6],
+    [3, 6, 9],
+  ];
+  expect(multiples).toEqual(result);
+
+  multiples = generatePrimeMultiples(3);
+  result = [
+    [1, 2, 3, 5],
+    [2, 4, 6, 10],
+    [3, 6, 9, 15],
+    [5, 10, 15, 25],
+  ];
+  expect(multiples).toEqual(result);
+
+  multiples = generatePrimeMultiples(5);
+  result = [
+    [1, 2, 3, 5, 7, 11],
+    [2, 4, 6, 10, 14, 22],
+    [3, 6, 9, 15, 21, 33],
+    [5, 10, 15, 25, 35, 55],
+    [7, 14, 21, 35, 49, 77],
+    [11, 22, 33, 55, 77, 121],
+  ];
+  expect(multiples).toEqual(result);
 });
