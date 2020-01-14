dab44bd672b827821b1b8a6986e5e867629e86e1
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-01-14 21:33:53 +0100

    Added docstrings

--- src/utils/primeMultiples.js
+++ src/utils/primeMultiples.js
@@ -1,5 +1,13 @@
 import { primeList } from './primeList';
 
+/**
+ * Generates the prime multiplication table.
+ * Adds a header and left hand marker to it so it looks like
+ * an orthodox multiplication table.
+ *
+ * @param {number} limit The limit of the prime list
+ * @returns {number[][]} The prime multiplication table as an array of array
+ */
 export const generatePrimeMultiples = limit => {
   if (limit < 2) {
     throw new Error('You need more than 1 number to generate multiples.');
@@ -16,7 +24,7 @@ export const generatePrimeMultiples = limit => {
   for (let i = 0; i < primes.length; i++) {
     const row = [];
     // add the prime that serves as left hand marker
-    row.push(primes[i])
+    row.push(primes[i]);
 
     for (let j = 0; j < primes.length; j++) {
       row.push(primes[i] * primes[j]);
