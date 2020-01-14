1b543305369e7353cd08426fad1a2a4c2e238b6e
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-01-14 20:24:56 +0100

    Tests for the prime range

--- src/utils/primeList.test.js
+++ src/utils/primeList.test.js
@@ -8,3 +8,13 @@ test('Correctly checks for primality', () => {
   expect(isPrime(49)).not.toBeTruthy();
   expect(isPrime(7919)).toBeTruthy();
 });
+
+test('Generates the right range of primes', () => {
+    let result = primeList(10);
+    const primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71];
+
+    expect(result).toEqual(primes.slice(0, 10));
+
+    result = primeList(20);
+    expect(result).toEqual(primes);
+  });
