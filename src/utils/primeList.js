0d34e8cd0f70ae32c3e194959234b79ff62f66af
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-01-14 19:59:50 +0100

    Added a docstring

--- src/utils/primeList.js
+++ src/utils/primeList.js
@@ -1,3 +1,9 @@
+/**
+ * Carries out a primality test.
+ *
+ * @param {number} num The number to be checked
+ * @returns {boolean} true/false to confirm primality
+ */
 const isPrime = num => {
   let lower = 2;
   const upper = Math.sqrt(num);
