926e5f1e3389e00619f6bd2b4f3c68eb09e93f29
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-01-14 21:10:39 +0100

    A placeholder test

---
+++ src/utils/primeMultiples.test.js
@@ -0,0 +1,6 @@
+import { generatePrimeMultiples } from './primeMultiples';
+
+test('Return the multiples of primes', () => {
+  let multiples = generatePrimeMultiples(5);
+  console.log(multiples);
+});
