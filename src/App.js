0cefaf55bd533f394320c4ce72d6d215b6b1bf7c
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-01-14 19:42:12 +0100

    Removed boiler plate markup

--- src/App.js
+++ src/App.js
@@ -3,10 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello</h1>
-      </header>
+    <div>
+      <h1>Hello React!</h1>
     </div>
   );
 }
