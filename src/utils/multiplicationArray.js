dc032614745d8002954c96d9680556bf171e847a
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-01-14 20:56:49 +0100

    Added an error check

--- src/utils/multiplicationArray.js
+++ src/utils/multiplicationArray.js
@@ -1,6 +1,9 @@
 import { primeList } from './primeList';
 
 export const generatePrimeMultiples = limit => {
-    const primes = primeList(limit);
-    console.log(primes);
-}
+  if (limit < 2) {
+    throw new Error('You need more than 1 number to generate multiples.');
+  }
+
+  const primes = primeList(limit);
+};
