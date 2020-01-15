404d9e03693448eab246b4d4abbcbe2cd36bb4f9
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-01-15 19:06:00 +0100

    Test the app

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  
+test('renders the app container', () => {
+  const { container } = render(<App />);
+  expect(container.firstChild).toHaveClass('container');
 });
