bdac3f85dec671f0a64b7d2c4654f9bf7f74651d
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-01-15 10:41:11 +0100

    Removed the conditional render from table row

--- src/components/row/TableRow.js
+++ src/components/row/TableRow.js
@@ -1,20 +1,11 @@
 import React from 'react';
 
-const TableRow = ({ row, isThead = false }) =>
-  isThead ? (
-    <thead>
-      <tr>
-        {row.map((item, index) => (
-          <th key={index}>{item > 1 && item}</th>
-        ))}
-      </tr>
-    </thead>
-  ) : (
-    <tr>
-      {row.map((item, index) => (
-        <td key={index}>{item}</td>
-      ))}
-    </tr>
-  );
+const TableRow = ({ row }) => (
+  <tr>
+    {row.map((item, index) => (
+      <td key={index}>{item}</td>
+    ))}
+  </tr>
+);
 
 export default TableRow;
