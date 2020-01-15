c55225b32a428e881aa50944334f3768fb7f1d5d
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-01-15 09:16:03 +0100

    Rendered the table

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import Table from './components/table/Table';
+
 function App() {
   return (
     <div>
-      <h1>Hello React!</h1>
+      <Table />
     </div>
   );
 }
