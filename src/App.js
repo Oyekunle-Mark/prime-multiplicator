88831c5b07ebede37ab9412a366f778f36bf8dfb
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-01-15 17:50:16 +0100

    Added a class name to the ap container

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { useState } from 'react';
 
 import Input from './components/input/Input';
 import Table from './components/table/Table';
+import './App.css';
 
 const App = () => {
   const [size, setSize] = useState(10);
 
   return (
-    <div>
+    <div className="container">
       <Input size={size} setSize={setSize} />
       <Table size={size} />
     </div>
